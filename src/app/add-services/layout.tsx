66d1c1fd4314013664cc27108546dbeee58e91f6
saket verma <saketverma831@gmail.com>
2024-01-19 01:30:31 +0530

    Canged Authify to calnflare everwhere

--- src/app/add-services/layout.tsx
+++ src/app/add-services/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <ProtectedRoute>
         <nav className="sticky top-0 z-[10] flex w-full items-center border-b bg-card  px-4 py-2 backdrop-blur-md ">
           <Link href="/dashboard" className="mr-auto">
-            Authify
+            Clanflare
           </Link>
 
           <ul className="flex list-none space-x-4">
--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav className="sticky top-0 z-[10] flex w-full items-center border-b bg-card  px-4 py-2 backdrop-blur-md ">
       <Link href="/dashboard" className="mr-auto">
-        Authify
+        Clanflare
       </Link>
 
       <ul className="flex list-none space-x-4">
