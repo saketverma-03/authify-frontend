b38453bfd971a141081864abbad4c93166052598
saket verma <saketverma831@gmail.com>
2024-02-03 01:23:12 +0530

    updated for api compatiblity

--- src/utils/backend/services/tag-manog.ts
+++ src/utils/backend/services/tag-manog.ts
@@ -2,24 +2,28 @@ import axios, { AxiosInstance, AxiosResponse } from "axios";
 import { IBackendResponse } from "../type";
 
 export interface ICreateServiceData {
-  name: string
-  roleIds: string[]
-  guildId: string
+  name: string;
+  roleIds: string[];
+  guildId: string;
 }
 export const createTmService = async (
   axios: AxiosInstance,
-  data: ICreateServiceData
+  data: ICreateServiceData,
 ): Promise<AxiosResponse<IBackendResponse<{}>>> =>
   await axios.post("/create-tm-service", data);
 
 export interface IGetManogesResData {
-  _id: string,
-  title: string
+  _id: string;
+  title: string;
 }
 
-export const getMangoes = async (axios: AxiosInstance): Promise<AxiosResponse<IBackendResponse<IGetManogesResData[]>>> =>
-  axios.get("customSolutions/gangstaPhilosophy/mangoes")
+export const getMangoes = async (
+  axios: AxiosInstance,
+  id: string,
+): Promise<AxiosResponse<IBackendResponse<IGetManogesResData[]>>> =>
+  axios.get(`customSolutions/gangstaPhilosophy/mangoes?serviceId=${id}`);
 
-
-export const addCustomSolution = async (axios: AxiosInstance): Promise<AxiosResponse<IBackendResponse<IGetManogesResData[]>>> =>
-  axios.get("customSolutions/gangstaPhilosophy/mangoes")
+export const addCustomSolution = async (
+  axios: AxiosInstance,
+): Promise<AxiosResponse<IBackendResponse<IGetManogesResData[]>>> =>
+  axios.get("customSolutions/gangstaPhilosophy/mangoes");
